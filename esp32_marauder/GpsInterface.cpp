601c56854ef52c9cea24836ab9df2e91376591ad
Just Call Me Koko <at.the.red.team@gmail.com>
2024-02-24 19:41:06 -0500

    Fix num sats on status bar

--- esp32_marauder/GpsInterface.cpp
+++ esp32_marauder/GpsInterface.cpp
@@ -38,10 +38,6 @@ void GpsInterface::begin() {
 
   delay(3900);
 
-  MicroNMEA::sendSentence(Serial2, "$PSTMFORCESTANDBY,00006");
-
-  delay(100);
-
   if (Serial2.available()) {
     Serial.println("GPS Attached Successfully");
     this->gps_enabled = true;
@@ -50,10 +46,8 @@ void GpsInterface::begin() {
       char c = Serial2.read();
       //Serial.print(c);
       //Pass the character to the library
-      Serial.print(c);
       nmea.process(c);
     }
-    Serial.println(nmea.getSentence());
   }
   else {
     this->gps_enabled = false;
@@ -538,6 +532,10 @@ String GpsInterface::getDatetime() {
 
 String GpsInterface::getNumSatsString() {
   return (String)num_sats;
+}
+
+int GpsInterface::getNumSats() {
+  return num_sats;
 }
 
 bool GpsInterface::getFixStatus() {
